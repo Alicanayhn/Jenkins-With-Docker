4140f1e813f07e591457add9de3f4befa15441e5
Ali Can Ayhan <alicanayhn@gmail.com>
2025-07-22 14:57:46 +0300

    Com

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -5,10 +5,9 @@ class BasicTestCase(unittest.TestCase):
     def test_home(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        
-        print('test merge deneme icin eklendi')
         self.assertEqual(response.status_code, 200) 
 
-
+        print('test merge deneme icin eklendi')
 if __name__ == '__main__':
     unittest.main()
+
