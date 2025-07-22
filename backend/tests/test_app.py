05476c91e4b9a7c4b7831474df0803495155c333
Ali Can Ayhan <alicanayhn@gmail.com>
2025-07-22 13:58:19 +0300

    deneme icin kod eklendi

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -5,7 +5,7 @@ class BasicTestCase(unittest.TestCase):
     def test_home(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(response.status_code, 300) 
 
 if __name__ == '__main__':
     unittest.main()
