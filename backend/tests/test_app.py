1ea921261c11f019bb4161de87e61081d79c0daa
Ali Can Ayhan <alicanayhn@gmail.com>
2025-07-22 15:03:25 +0300

    nothing func eklendi

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -7,8 +7,11 @@ class BasicTestCase(unittest.TestCase):
         response = tester.get('/')
         self.assertEqual(response.status_code, 200) 
 
-
         print('test merge deneme icin eklendi')
+
+def nothing(x):
+    pass
+
 if __name__ == '__main__':
     unittest.main()
 
