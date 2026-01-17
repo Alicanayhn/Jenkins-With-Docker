105189b0296ffe5649f677aa95c42af904850dda
AliCanAyhan <alicanayhn@gmail.com>
2026-01-17 17:33:05 +0300

    feat: login icin css düzenlendi

--- backend/app.py
+++ backend/app.py
@@ -13,11 +13,13 @@ def api_control():
     return jsonify(data)
 
 
+# Health check endpoint
 @app.route("/health", methods=["GET"])
 def health_check():
     return "OK", 200
 
 
+# Readiness check endpoint
 @app.route("/ready", methods=["GET"])
 def readiness_check():
     return "READY", 200
@@ -29,6 +31,7 @@ def test():
     return "Application is running", 200
 
 
+# Simple route for the root URL
 @app.route("/")
 def index():
     return "Uygulama Calisiyor!"
