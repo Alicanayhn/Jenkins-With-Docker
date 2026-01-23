99439a14778784a415513528c79d0bfd510f365a
AliCanAyhan <alicanayhn@gmail.com>
2026-01-23 09:53:52 +0300

    test: new unit tests added

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,11 +1,19 @@
 import unittest
 from app import app
 
+
 class BasicTestCase(unittest.TestCase):
     def test_home(self):
         tester = app.test_client(self)
-        response = tester.get('/')
-        self.assertEqual(response.status_code, 200) 
+        response = tester.get("/")
+        self.assertEqual(response.status_code, 200)
 
-if __name__ == '__main__':
+    def unit_test_example(self):
+        tester = app.test_client(self)
+        response = tester.get("/unit-test-example")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Unit Test Example", response.data)
+
+
+if __name__ == "__main__":
     unittest.main()
